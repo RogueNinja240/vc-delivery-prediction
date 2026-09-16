61c36519d79efdec0790bc26fb9c3e00daf60975
RogueNinja240 <paulsunil0016@gmail.com>
2026-09-16 13:16:42 +0530

    rollbacked-ui

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start FastAPI in the background (the '&' makes it run in the background)
-uvicorn app:app --host 0.0.0.0 --port 8000 &
-
-# Start Streamlit in the foreground. 
-# We use Render's default $PORT, or 8501 if running locally.
-PORT=${PORT:-8501}
-streamlit run frontend.py --server.port $PORT --server.address 0.0.0.0
